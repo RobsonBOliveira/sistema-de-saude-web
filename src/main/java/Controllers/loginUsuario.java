b8f68c289f0dfbcd9cce87b7c3ed86c863de28f9
Luiz Carlos <lcarfernandes05@gmail.com>
2024-10-16 23:05:30 -0300

    Implementação do login e senha junto de redirecionamento caso correto e caso incorreto.

--- src/main/java/Controllers/loginUsuario.java
+++ src/main/java/Controllers/loginUsuario.java
@@ -11,26 +11,36 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-@WebServlet ("/loginUsuario")
+@WebServlet("/loginUsuario")
 public class loginUsuario extends HttpServlet {
-    public loginUsuario(){
+
+    public loginUsuario() {
         super();
     }
 
     private Connection con = null;
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         String login = request.getParameter("login");
         String senha = request.getParameter("senha");
+        boolean conectado = false;
 
         try {
             Class.forName("org.postgresql.Driver");
-            String URL ="jdbc:postgresql://localhost:5432/sistemasaude";
-            con = DriverManager.getConnection(URL,login, senha);
-
+            String URL = "jdbc:postgresql://localhost:5432/sistemasaude";
+            con = DriverManager.getConnection(URL, login, senha);
+            conectado = true;  // Se a conexão for bemsucedida
         } catch (SQLException | ClassNotFoundException e) {
-            System.out.println(e);
+            System.out.println("Erro na conexão: " + e.getMessage());
+        }
+
+        // Redireciona com base no status da conexão
+        if (conectado) {
+            response.sendRedirect("teste.html");
+        } else {
+            response.sendRedirect("cadastrarPaciente.html");
         }
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -2,19 +2,15 @@
 <html lang="pt-br" xmlns:br="http://www.w3.org/1999/html">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="estilo.css">
     <title>Login de Usuarios</title>
 </head>
 <body>
     <form action="loginUsuario" method="post">
         Login: <input type="text" name="login"> <br>
         Senha: <input type="password" name="senha"> <br>
-        <input type="submit" value="Conectar" onclick="">
+        <input type="submit" value="Conectar">
     </form>
-
-    <form action="cadastrarUsuario.html" method="post">
-        <input type="submit" value="Cadastrar Usuário">
-    </form>
-
-
+    <a href="cadastrarUsuario.html"><input type="button" value="Cadastrar Novo Usuario"></a>
 </body>
 </html>
